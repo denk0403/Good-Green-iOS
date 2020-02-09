4994ef9a7d2ec9a9fef9ee635313bd11aa6ba786
jrosen081 <jrosen081@gmail.com>
2020-02-09 05:25:14 -0500

    Doing some fun changes

--- Good Green/FeedView.swift
+++ Good Green/FeedView.swift
@@ -9,9 +9,54 @@
 import SwiftUI
 
 struct FeedView: View {
+	@State private var isLoading = true
+	@State private var hasError = false
+	@State private var feedObjects = [FeedObject]()
+	@Environment(\.appService) var appService: AppService
+	
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+		if hasError {
+			return AnyView(self.errorView)
+		}
+		return AnyView(self.loadingView)
     }
+	
+	var errorView: some View {
+		Button(action: { withAnimation { self.isLoading = true }}) {
+			Text("There was an error retrieving your data.")
+		}
+	}
+	
+	var loadingView: some View {
+		LoadingView(isLoading: self.isLoading) {
+			ZStack {
+				Color(Constants.whiteSmoke).edgesIgnoringSafeArea(.all)
+				VStack {
+					Image("logo").resizable().scaledToFit().padding()
+					self.feedObjects.isEmpty ? AnyView(self.emptyView) : AnyView(self.nonEmptyView)
+				}
+			}
+		}.onAppear {
+			self.appService.getFeed(offset: 0) {
+				self.isLoading = false
+				self.hasError = $0 == nil
+				self.feedObjects = $0 ?? []
+			}
+		}
+	}
+	
+	var emptyView: some View {
+		VStack {
+			Spacer()
+			Text("You have no feed events. Add people by searching for them below.")
+			Image(systemName: "ArrowDown").resizable().scaledToFill()
+			Spacer()
+		}
+	}
+	
+	var nonEmptyView: some View {
+		FeedListView(feedObjects: self.feedObjects)
+	}
 }
 
 struct FeedView_Previews: PreviewProvider {
